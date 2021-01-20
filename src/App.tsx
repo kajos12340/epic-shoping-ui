21a4f68722792bbcd4c1daa8f93535183be6d3e3
kajos12340 <piotr.kajka.96@gmail.com>
2021-01-20 19:47:34 +0100

    axios setup pt2

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
-import { setupAxiosBaseUrl } from './helpers/axios';
+import { setToken, setupAxiosBaseUrl } from './helpers/axios';
 
 setupAxiosBaseUrl(process.env.REACT_APP_BE_ADDRESS);
+setToken('tokeniszce');
 
 const App = () => {
+  const [response, setResponse] = useState<Object>();
+
   const basicFetch = async () => {
     const resp = await axios.get('/test');
     console.log(resp);
+    setResponse(resp);
   };
 
   return (
@@ -20,6 +24,11 @@ const App = () => {
       <button type="button" onClick={basicFetch}>
         Fetch
       </button>
+      <pre>
+        <code>
+          {JSON.stringify(response, null, 2)}
+        </code>
+      </pre>
     </header>
   );
 };
