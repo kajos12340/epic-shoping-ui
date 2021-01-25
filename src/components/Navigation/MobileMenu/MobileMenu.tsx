da3b08e489c35b9a73fac8f17910934157917a4c
kajos12340 <piotr.kajka.96@gmail.com>
2021-01-25 19:33:38 +0100

    menu implementation

--- src/components/Navigation/MobileMenu/MobileMenu.tsx
+++ src/components/Navigation/MobileMenu/MobileMenu.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-import { useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import { IMenuOption } from '../Navigation';
 
@@ -8,33 +8,28 @@ import { BottomNavigation } from './MobileNavigation.styles';
 
 interface MobileMenuProps {
   menuOptions: IMenuOption[],
+  currentActiveTab: string
 }
 
-const MobileMenu = ({ menuOptions }: MobileMenuProps) => {
-  const [value, setValue] = React.useState(0);
-  const location = useLocation();
-  console.log('location', location);
+const MobileMenu = ({ menuOptions, currentActiveTab }: MobileMenuProps) => (
+  <div>
+    <BottomNavigation
+      value={currentActiveTab}
+      showLabels
+    >
+      {menuOptions.map((menuItem) => (
+        <BottomNavigationAction
+          component={Link}
+          to={menuItem.to}
+          label={menuItem.label}
+          icon={menuItem.icon}
+          key={menuItem.to}
+          value={menuItem.tabName}
+        />
+      ))}
+    </BottomNavigation>
+  </div>
 
-  return (
-    <div>
-      <BottomNavigation
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-        showLabels
-      >
-        {menuOptions.map((menuItem) => (
-          <BottomNavigationAction
-            label={menuItem.label}
-            icon={menuItem.icon}
-            key={menuItem.to}
-          />
-        ))}
-      </BottomNavigation>
-    </div>
-
-  );
-};
+);
 
 export default MobileMenu;
--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,4 +1,5 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 
 import ChatBubbleOutlineOutlinedIcon from '@material-ui/icons/ChatBubbleOutlineOutlined';
 import ListOutlinedIcon from '@material-ui/icons/ListOutlined';
@@ -12,6 +13,7 @@ export interface IMenuOption {
   to: string,
   icon: ReactNode,
   label: string,
+  tabName: string,
 }
 
 const menuOptions: IMenuOption[] = [
@@ -19,29 +21,54 @@ const menuOptions: IMenuOption[] = [
     icon: <ListOutlinedIcon />,
     to: '/shopping/lists',
     label: 'Listy zakupów',
+    tabName: 'shopping',
   },
   {
     icon: <ChatBubbleOutlineOutlinedIcon />,
     to: '/messages',
     label: 'Wiadomości',
+    tabName: 'messages',
   },
   {
     icon: <PermIdentityOutlinedIcon />,
     to: '/user',
     label: 'Moje konto',
+    tabName: 'user',
   },
 ];
 
 const Navigation = () => {
+  const [currentActiveTab, setCurrentActiveTab] = useState('');
+
+  const location = useLocation();
+
+  useEffect(() => {
+    setCurrentActiveTab('');
+
+    switch (true) {
+      case location.pathname.includes('user'):
+        setCurrentActiveTab('user');
+        break;
+      case location.pathname.includes('shopping'):
+        setCurrentActiveTab('shopping');
+        break;
+      case location.pathname.includes('messages'):
+        setCurrentActiveTab('messages');
+        break;
+      default:
+        setCurrentActiveTab('shopping');
+    }
+  }, [location]);
+
   const mobileMenu = (
     <MobileMenuContainer>
-      <MobileMenu menuOptions={menuOptions} />
+      <MobileMenu menuOptions={menuOptions} currentActiveTab={currentActiveTab} />
     </MobileMenuContainer>
   );
 
   const screenMenu = (
     <ScreenMenuContainer>
-      <ScreenMenu menuOptions={menuOptions} />
+      <ScreenMenu menuOptions={menuOptions} currentActiveTab={currentActiveTab} />
     </ScreenMenuContainer>
   );
 
