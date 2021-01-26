a874d2b10e38ba68410bbfcd4bbf19fae2eeae47
kajos12340 <piotr.kajka.96@gmail.com>
2021-01-26 14:07:31 +0100

    form custom hook

---
+++ src/hooks/useForm/useForm.tsx
@@ -0,0 +1,122 @@
+import {
+  useState, ChangeEvent, FormEvent, useEffect,
+} from 'react';
+
+import { IValidator } from '../../components/Input/Input';
+
+interface IFormDataItem {
+  value: string,
+  error: string,
+  checked: boolean,
+}
+
+type IFormData = {
+  [name: string]: IFormDataItem
+}
+
+export interface IForm {
+  onChange(e: ChangeEvent<HTMLInputElement>): void,
+  validate(validators: Function[], name: string): IValidator,
+  getValue(name: string): string | boolean,
+  hasAnyError(): boolean,
+  getFormValues(): IFormData,
+  getValidationResult(name: string): string,
+  submit: (e: FormEvent, validators: { [name: string]: IValidator }) => boolean | IFormData,
+}
+
+interface IInitialValues {
+  [name: string]: string | boolean | number,
+}
+
+const UseForm = (initialValues?: IInitialValues): IForm => {
+  const [formData, setFormData] = useState<IFormData>({});
+
+  useEffect(() => {
+    if (initialValues) {
+      const parsedInitialValues: IFormData = Object.entries(initialValues)
+        .reduce((acc, [key, value]) => {
+          let newValue;
+          let checked;
+          if (typeof value === 'boolean') {
+            checked = value;
+          } else {
+            newValue = `${value}`;
+          }
+          return {
+            ...acc,
+            [key]: {
+              value: newValue || '',
+              error: '',
+              checked: checked || false,
+            },
+          };
+        }, {});
+      setFormData(parsedInitialValues);
+    }
+  }, [initialValues]);
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (!e.target) return;
+
+    const fieldName = e.target.name;
+    const { value } = e.target;
+    const { checked } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [fieldName]: {
+        value,
+        checked,
+        error: '',
+      },
+    }));
+  };
+
+  const validate = (validators: Function[], name: string): IValidator => () => {
+    const validationResult = validators
+      .map((validator) => validator(formData[name]?.value))
+      .find((i) => i);
+    setFormData((prev) => ({
+      ...prev,
+      [name]: {
+        ...prev[name],
+        error: validationResult,
+      },
+    }));
+  };
+
+  const getValidationResult = (name: string): string => (
+    formData[name]?.error
+  );
+
+  const getValue = (name: string): string | boolean => (
+    formData[name]?.value || formData[name]?.checked
+  );
+
+  const hasAnyError = (): boolean => Object.values(formData).some((item) => item.error);
+
+  const getFormValues = () => formData;
+
+  const submit = (e: FormEvent, validators: { [name: string]: IValidator }) => {
+    e.preventDefault();
+    Object.values(validators).forEach((validator) => {
+      validator(null);
+    });
+    if (hasAnyError()) {
+      return false;
+    }
+
+    return getFormValues();
+  };
+
+  return {
+    onChange,
+    validate,
+    getValue,
+    hasAnyError,
+    getFormValues,
+    getValidationResult,
+    submit,
+  };
+};
+
+export default UseForm;
