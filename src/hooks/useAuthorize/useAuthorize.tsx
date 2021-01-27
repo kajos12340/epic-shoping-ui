221a028912db9f5c81ba74770be93fb46ed4db27
kajos12340 <piotr.kajka.96@gmail.com>
2021-01-27 20:00:31 +0100

    useAuthorize hook added

---
+++ src/hooks/useAuthorize/useAuthorize.tsx
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { useSnackbar } from 'notistack';
+
+import { RootReducerState } from '../../store/store';
+
+const UseAuthorize = () => {
+  const user = useSelector((state: RootReducerState) => state.user);
+  const { enqueueSnackbar } = useSnackbar();
+  const history = useHistory();
+
+  useEffect(() => {
+    if (!user || !localStorage.getItem('token')) {
+      enqueueSnackbar('Zaloguj się aby uzyskać dostęp do tej zakładki!', {
+        variant: 'error',
+      });
+      history.push('/user/login');
+    }
+  }, [user]);
+
+  return null;
+};
+
+export default UseAuthorize;
