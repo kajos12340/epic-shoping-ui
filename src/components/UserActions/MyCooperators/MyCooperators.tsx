8d0415155be3b6ed1c3fbcbb833aad26f5bfaadb
kajos12340 <piotr.kajka.96@gmail.com>
2021-01-27 21:19:33 +0100

    My cooperators done

--- src/components/UserActions/MyCooperators/MyCooperators.tsx
+++ src/components/UserActions/MyCooperators/MyCooperators.tsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import {
+  List, ListItem, ListItemAvatar, ListItemText, Avatar, Divider,
+} from '@material-ui/core';
 
 const MyCooperators = () => (
-  <div>
-    MyCooperators
-  </div>
+  <List>
+    <ListItem>
+      <ListItemAvatar>
+        <Avatar>
+          M
+        </Avatar>
+      </ListItemAvatar>
+      <ListItemText primary="Majaczek" secondary="Data rejestracji: 24.09.2020" />
+    </ListItem>
+    <Divider />
+    <ListItem>
+      <ListItemAvatar>
+        <Avatar color="primary">
+          P
+        </Avatar>
+      </ListItemAvatar>
+      <ListItemText primary="Piotraczek" secondary="Data rejestracji: 24.11.2020" />
+    </ListItem>
+    <Divider />
+    <ListItem>
+      <ListItemAvatar>
+        <Avatar>
+          T
+        </Avatar>
+      </ListItemAvatar>
+      <ListItemText primary="Testowy" secondary="Data rejestracji: 25.09.2020" />
+    </ListItem>
+  </List>
 );
 
 export default MyCooperators;
