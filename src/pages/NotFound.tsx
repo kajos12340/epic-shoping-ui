0948510566645b6dd2d1821f60d610a583671196
kajos12340 <piotr.kajka.96@gmail.com>
2021-01-31 22:57:41 +0100

    404 message added

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Grid, Typography } from '@material-ui/core';
 
 const NotFound = () => (
-  <div>
-    NotFound
-  </div>
+  <Grid container justify="center">
+    <Typography variant="h1">
+      404 ;(
+    </Typography>
+  </Grid>
 );
 
 export default NotFound;
