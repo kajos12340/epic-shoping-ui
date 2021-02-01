174187465db51ed4e71f123888d857f48684dff7
kajos12340 <piotr.kajka.96@gmail.com>
2021-02-01 16:11:03 +0100

    Basic validator test added

---
+++ src/validators/Validators.test.ts
@@ -0,0 +1,10 @@
+import * as Validators from './Validators';
+
+describe('validators', () => {
+  it('validate required', () => {
+    const invalidText = 'Pole jest wymagane!';
+    expect(Validators.required('')).toBe(invalidText);
+    expect(Validators.required(false)).toBe(invalidText);
+    expect(Validators.required('value')).toBe('');
+  });
+});
